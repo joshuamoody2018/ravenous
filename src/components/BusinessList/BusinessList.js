ac77e385b00bfbbd1217441f50afc0a5227aecba
Joshua S <58367886+joshuamoody2018@users.noreply.github.com>
2020-05-01 13:54:57 -0400

    Project Complete and working

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,12 +3,11 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component{
-
     render(){
         return(
             <div className="BusinessList">
                 {this.props.businesses.map(business => {
-                        return <Business business={business}/>;
+                        return <Business business={business} key={business.id}/>;
                         }
                     )
                 }
---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'QaVPdF4sgt4Z1b9ZH1CYvAJu3uGofUm3yg8V0YsLsxro4BxAsV9IcBpjOtLmLOluI91lGsVbsTQi8YxpZJprfkwXjIBIqaetTtSFNQB-YWiyBHgroCsHGLZxfkesXnYx';
+
+const Yelp = {
+    async search(term, location, sortBy){
+        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}&limit=10`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        });
+        const jsonResponse = await response.json();
+        if (jsonResponse.businesses) {
+            return jsonResponse.businesses.map(business => {
+                return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                };
+            });
+        }
+    },
+};
+
+export default Yelp;
